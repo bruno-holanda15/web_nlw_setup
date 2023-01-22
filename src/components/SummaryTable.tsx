1c5399092d064989d5cfb8b8b8ededa903dbd43c
Bruno Holanda <br.holanda15@gmail.com>
2023-01-21 22:40:19 -0300

    Day 03

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -30,11 +30,15 @@ export function SummaryTable() {
 
             <div className="grid grid-rows-7 grid-flow-col gap-3">
                 {summaryDates.map(date => {
-                    return <HabitDay key={date.toString()}/>
+                    return (<HabitDay 
+                        key={date.toString()} 
+                        amount={5} 
+                        completed={Math.round(Math.random() * 5)} 
+                    />)
                 })}
                 {amountOfDaysToFill > 0 && Array.from({length: amountOfDaysToFill}).map((_,i) => {
                     return (
-                        <div className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg opacity-20 cursor-not-allowed" />
+                        <div className="w-10 h-10 bg-zinc-900 border border-zinc-800 rounded-lg opacity-10 cursor-not-allowed" />
                     )
                 })}
             </div>
